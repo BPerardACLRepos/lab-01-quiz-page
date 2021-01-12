dcf1a89eda511d73460bd754cf6e215f664bcb04
Brandon Perard <brandon.perard@gmail>
2021-01-11 19:19:43 -0800

    add prompt & save user name, and user confirm with return to exit callback function in app.js; add userAnswerDiv to index.html

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ const userAnswerDiv = document.querySelector('#user-results');
 
 quizStartButton.addEventListener('click', () => {
     let correctAnswers = 0;
-    console.log(correctAnswers)
+    const firstName = prompt(`What's your first name?`);
+    const lastName = prompt(`What's your last name?`);
+
+    const userConfirm = confirm('Are you ready for this?');
+    if (!userConfirm) {
+        return;
+    }
+    console.log('yollo')
 })
